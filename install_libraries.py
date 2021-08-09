9dc65348b72bc6ffc4d5765bf9d5dc1c8d2924c5
Alexey Pakhomov <alexeyt101@mail.ru>
2021-08-09 21:08:03 +0300

    Changed .gitignore, tested input data

--- install_libraries.py
+++ install_libraries.py
@@ -1,21 +1,10 @@
 import os
-from sys import platform
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if platform == 'win32':
-    os.system(
-        f'cd {BASE_DIR} &\
-        python -m venv env &\
-        {BASE_DIR}\\env\\Scripts\\activate &\
-        pip install -r {BASE_DIR}\\requirements.txt'
-    )
-elif platform == 'linux':
-    os.system(
-        f'cd {BASE_DIR} &\
-        python -m venv env &\
-        source {BASE_DIR}/env/bin/activate &\
-        pip install -r {BASE_DIR}/requirements.txt'
-    )
-else:
-    print('Ваша операционная система не распознана, выполните инициализацию виртуального окружения и установку библиотек вручную')
+os.system(
+    f'cd {BASE_DIR} &\
+    python -m venv env &\
+    {BASE_DIR}\\env\\Scripts\\activate &\
+    pip install -r {BASE_DIR}\\requirements.txt'
+)
--- run_app.py
+++ run_app.py
@@ -1,19 +1,10 @@
 import os
-from sys import platform
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-if platform == 'win32':
-    os.system(
-        f'cd {BASE_DIR} &\
-        {BASE_DIR}\\env\\Scripts\\activate &\
-        cd {BASE_DIR}\\app &\
-        python {BASE_DIR}\\app\\gui_to_processing.py'
-    )
-elif platform == 'linux':
-    os.system(
-        f'cd {BASE_DIR} &\
-        {BASE_DIR}/env/bin/activate &\
-        cd {BASE_DIR}/app &\
-        python {BASE_DIR}/app/gui_to_processing.py'
-    )
+os.system(
+    f'cd {BASE_DIR} &\
+    {BASE_DIR}\\env\\Scripts\\activate &\
+    cd {BASE_DIR}\\app &\
+    python {BASE_DIR}\\app\\gui_to_processing.py'
+)
